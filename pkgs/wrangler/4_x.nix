d80cde7dc879f284ff496cb6f21565d662de50c0
Ryan Omasta <git@ryand.ca>
2026-01-22 21:18:13 -0700

    wrangler: 4.59.3 -> 4.60.0

--- pkgs/wrangler/4_x.nix
+++ pkgs/wrangler/4_x.nix
@@ -1,5 +1,5 @@
 import ./generic.nix {
-  version = "4.59.3";
-  hash = "sha256-DKKngT58p8x+Qzl550468JDOQuymQmzPwkLL/iB7Aa4=";
-  pnpmDepsHash = "sha256-r3J4xNZJlWmNtnpLfx/eKK3TDKcrjZmR7oxp6TDuwxg=";
+  version = "4.60.0";
+  hash = "sha256-lRdQrUgEr7KS/05BXZW7h3JS91d3gM8w+RLlaLC98RU=";
+  pnpmDepsHash = "sha256-TML3nExDrfA+F8g4rgFavnjuC9iq2oEBNXmHkUTdSJU=";
 }
