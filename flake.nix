30eadc2e6bb3bc8c7efbded906ae5021c69214e5
Ryan Omasta <git@ryand.ca>
2026-07-15 03:51:49 -0600

    chore: get rid of flake-parts

--- flake.nix
+++ flake.nix
@@ -1,52 +1,40 @@
 {
   description = "Wrangler, the CLI for Cloudflare Workers, packaged as a nix flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-  };
+  inputs.nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } (
-      { ... }:
-      {
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-          "aarch64-darwin"
-        ];
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs (builtins.filter (x: x != "x86_64-darwin") lib.systems.flakeExposed);
+    in
+    rec {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt);
 
-        perSystem =
-          {
-            pkgs,
-            ...
-          }:
-          rec {
-            formatter = pkgs.nixfmt;
+      packages = forAllSystems (system: rec {
+        wrangler_4 = nixpkgs.legacyPackages.${system}.callPackage ./pkgs/wrangler/4_x.nix { };
 
-            packages = rec {
-              wrangler_4 = pkgs.callPackage ./pkgs/wrangler/4_x.nix { };
+        wrangler = wrangler_4;
+        default = wrangler;
+      });
 
-              wrangler = wrangler_4;
-              default = wrangler;
-            };
-
-            devShells = {
-              default = pkgs.mkShell {
-                packages = [
-                  pkgs.nixfmt
-                  pkgs.nodejs
-                  pkgs.pnpm
-                ];
-              };
-            };
-
-            checks = packages // devShells;
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nixfmt
+              nodejs
+              pnpm
+            ];
           };
-      }
-    );
+        }
+      );
+
+      checks = packages // devShells;
+    };
 }
